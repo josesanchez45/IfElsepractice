0246a2d2f1eb82911f3122395bcaddf092a4e522
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-10 12:14:24 -0700

    initial commit

---
+++ SelectionStatementExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿// Exercise 1
+var favNumber = 25;
+
+
+Console.WriteLine(" Can you guess my favorite number?");
+Console.WriteLine("Type in your guess below (between 1 and 100) : ");
+var userInput = int.Parse(Console.ReadLine());
+
+if (userInput == favNumber)
+{
+    Console.WriteLine("You guessed it!"); 
+}
+else if (userInput < favNumber)
+{
+    Console.WriteLine("Too low!");
+}
+else
+{
+    Console.WriteLine("Too High!");
+}
+//Exercise 2
